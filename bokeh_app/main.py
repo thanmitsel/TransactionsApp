7aedb7273491520948a1860777844571b09c66f9
thanmitsel <thanmitsel@gmail.com>
2021-03-03 17:24:18 +0000

    load new dataset

--- bokeh_app/main.py
+++ bokeh_app/main.py
@@ -2,6 +2,7 @@
 import pandas as pd
 
 # os methods for manipulating paths
+import os
 from os.path import dirname, join
 
 # Bokeh basics
@@ -12,30 +13,52 @@ from bokeh.models.widgets import Tabs
 from scripts.overview import overview_tab
 from scripts.expenses import expenses_tab
 
+
+def read_files(app_dir, data_dir):
+    df = pd.DataFrame()
+    for afile in os.listdir(join(app_dir,data_dir)):
+        if '-' in afile:
+            path_file = join(dirname(__file__), data_dir, afile)
+            temp_df = pd.read_csv(path_file)
+            df = df.append(temp_df)
+    return df
+
+
+
 # Read inial file
 in_name = 'transactions.csv'
 data_dir = 'data'
-trans = pd.read_csv(join(dirname(__file__), data_dir, in_name))
+app_dir = 'bokeh_app'
+gr_trans = pd.read_csv(join(dirname(__file__), data_dir, in_name))
+uk_trans = read_files(app_dir, data_dir)
 
-# Define the parameters for plotting
-x_name = 'Category' # existing col
-measure = 'Amount' # existing col
-date_name = 'Posted date' # existing col
+# Define the parameters for plotting on one card
+x_name_gr = 'Category' # existing col
+measure_gr = 'Amount' # existing col
+date_name_gr = 'Posted date' # existing col
+
+# Define the parameters for plotting on the other card
+x_name_uk = 'Transaction description' # existing col
+measure_uk = 'Debit Amount' # existing col
+date_name_uk = 'Transaction date' # existing col
+
+# columns names for metrics
 y_name = 'Total'
 t_count = 'count'
 
 
 # Convert to datetime
-trans[date_name] = pd.to_datetime(trans[date_name], format = "%d/%m/%Y")
+gr_trans[date_name_gr] = pd.to_datetime(gr_trans[date_name_gr], format = "%d/%m/%Y")
+uk_trans[date_name_uk] = pd.to_datetime(uk_trans[date_name_uk], format = "%Y-%m-%d")
 
 # Segment to deposits and expenses
-dep = trans.loc[trans[measure] > 0].copy()
-exp = trans.loc[trans[measure] < 0].copy()
-exp[measure] = exp[measure].abs()
+dep = gr_trans.loc[gr_trans[measure_gr] > 0].copy()
+exp = gr_trans.loc[gr_trans[measure_gr] < 0].copy()
+exp[measure_gr] = exp[measure_gr].abs()
 
 # Create each of the tabs
-tab1 = overview_tab(trans, x_name, date_name, measure)
-tab2 = expenses_tab(exp, x_name, y_name, measure, t_count)
+tab1 = overview_tab(gr_trans, x_name_gr, date_name_gr, measure_gr)
+tab2 = expenses_tab(exp, x_name_gr, y_name, measure_gr, t_count)
 
 # Put all the tabs into one application
 tabs = Tabs(tabs = [tab1, tab2])
